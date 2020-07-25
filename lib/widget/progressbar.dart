9dd83722cb85c3be21ec6ffb29bdf93f91615d46
luozhanming <459557210@qq.com>
2020-07-25 15:31:20 +0800

    大修改暂存

--- lib/widget/progressbar.dart
+++ lib/widget/progressbar.dart
@@ -1,2 +1,47 @@
 
 
+import 'package:flutter/widgets.dart';
+
+class ProgressBar extends StatelessWidget{
+
+
+  final int max;
+  final int progress;
+  final Color backgroundColor;
+  final Color progressColor;
+  final double height;
+
+
+  ProgressBar({this.max, this.progress, this.backgroundColor, this.progressColor,
+      this.height});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomPaint(
+
+    );
+  }
+
+}
+
+
+class PrgressBarPainter extends CustomPainter{
+
+
+  final int max;
+  final int progress;
+  final Color backgroundColor;
+  final Color progressColor;
+  final double height;
+
+
+  @override
+  void paint(Canvas canvas, Size size) {
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    throw UnimplementedError();
+  }
+
+}
