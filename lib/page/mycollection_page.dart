a6b88841404f91f463e759d4bd9c681279b53a31
罗展鸣 <luozhanming@ava.com.cn>
2020-12-23 18:19:02 +0800

    收藏模块推进，定位collection_page

--- lib/page/mycollection_page.dart
+++ lib/page/mycollection_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:wanandroid/common/base/base_state.dart';
@@ -7,19 +9,13 @@ import 'package:wanandroid/common/base/base_viewmodel.dart';
 import 'package:wanandroid/common/styles.dart';
 import 'package:wanandroid/generated/l10n.dart';
 import 'package:wanandroid/model/artical.dart';
-import 'package:wanandroid/model/global_state.dart';
 import 'package:wanandroid/model/pager.dart';
 import 'package:wanandroid/model/resource.dart';
-import 'package:wanandroid/page/webview/webview_page.dart';
 import 'package:wanandroid/repository/collect_repository.dart';
-import 'package:wanandroid/widget/artical_item_widget.dart';
 import 'package:wanandroid/widget/common_appbar.dart';
-import 'package:provider/provider.dart';
 
 class MyCollectionPage extends StatefulWidget {
-
   static const String NAME = "/mycollection";
-
 
   @override
   _MyCollectionPageState createState() => _MyCollectionPageState();
@@ -27,8 +23,6 @@ class MyCollectionPage extends StatefulWidget {
 
 class _MyCollectionPageState
     extends BaseState<MyCollectionPage, _MyCollectionViewModel> {
-
-
   @override
   Widget buildBody(BuildContext context) {
     return Scaffold(
@@ -42,22 +36,64 @@ class _MyCollectionPageState
     return _MyCollectionViewModel(context);
   }
 
-
   PreferredSize _buildAppBar(BuildContext context) {
     return MyAppBar(
       leadingIcon: Icons.arrow_back,
       onLeadingIconTap: () => Navigator.pop(context),
       widget: Text(
-        S
-            .of(context)
-            .my_collection,
+        S.of(context).my_collection,
         style: TextStyles.titleTextStyle,
       ),
     );
   }
 
   Widget _buildCollectionList() {
-    
+    return Builder(
+      builder: (context) {
+        var itemCount = context.select<_MyCollectionViewModel, int>(
+            (value) => value.articals.length);
+        return SmartRefresher(
+            //child不能是Builder
+            controller: mViewModel._refreshController,
+            enablePullUp: true,
+            onRefresh: () {
+              mViewModel.loadMyCollections(false);
+            },
+            onLoading: () {
+              mViewModel.loadMyCollections(true);
+            },
+            child: ListView.builder(
+                itemBuilder: (context, index) {
+                  if (itemCount == 0) {
+                    return _buildEmptyDataView(context);
+                  } else {
+                    return _buildListItem(context,index);
+                  }
+                },
+                itemCount:  1 ));
+      },
+    );
+  }
+
+  Widget _buildEmptyDataView(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setWidth(360),
+      child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Center(child: Text("sdfsdf"))
+          ]),
+    );
+  }
+
+  Widget _buildListItem(BuildContext context,int index) {
+    Artical articals = context.select<_MyCollectionViewModel, Artical>(
+            (value) => value.articals[index]);
+    return Builder(
+      builder: (context){
+        return Container()
+      },
+    );
   }
 }
 
@@ -68,7 +104,6 @@ class _MyCollectionViewModel extends BaseViewModel {
   CompositeSubscription _subscriptions;
   Resource<Pager<Artical>> articalPageRes;
   List<Artical> articals = [];
-
 
   @override
   void initState() {
@@ -92,8 +127,7 @@ class _MyCollectionViewModel extends BaseViewModel {
         page = articalPageRes.data.curPage;
       }
     }
-    _subscriptions.add(_repository.loadMyCollections(page)
-        .listen((event) {
+    _subscriptions.add(_repository.loadMyCollections(page).listen((event) {
       articalPageRes = event;
       var page = articalPageRes.data;
       if (!isLoadMore) {
@@ -122,5 +156,4 @@ class _MyCollectionViewModel extends BaseViewModel {
       }
     }));
   }
-
 }
